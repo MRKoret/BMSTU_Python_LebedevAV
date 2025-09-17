f76ade706f49a05b98d05fcce273cf6937f3eeb8
Александр <alex_lebedmrk24@mail.ru>
2025-09-17 20:35:06 +0300

    Merge remote-tracking branch 'origin/master'

--- laboratory_work_02/lab_work_02_01.py
+++ laboratory_work_02/lab_work_02_01.py
@@ -1,29 +1,20 @@
 from math import *
+
 x=float(input("Введите значение для x: "))
-if x < -10:
+
+if x <= -8:
     y = -3
-    print(y)
-if -10<=x<-8:
-    y=-3
-    print(y)
-if x == -8 or x == 0:
-    y =-3
-    print(y)
+elif -8 < x < -3:
+    y = (3 / 5) * x + 9 / 5
 if x == -3 or x==3:
     y=0
-    print(y)
-elif -8<x<-3:
-    y = (3/5)*x + 9/5
-    print(y)
-elif -3<x<3 and x !=0:
+elif -3<x<3:
     y = -sqrt(9-x**2)
-    print(y)
+elif 3 < x < 5:
+    y = x - 3
 elif x==5:
     y = 3
-    print(y)
-elif 3<x<5:
-    y = x - 3
-    print(y)
 else:
     y = 3
-    print(y)
+
+print("X = {0:.2f} Y = {1:.2f}".format(x, y))
