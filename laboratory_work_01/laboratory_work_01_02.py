a2a189ee3fe3ca64e974b6cb3047ee9b9e7e35e4
Александр <alex_lebedmrk24@mail.ru>
2025-09-25 19:38:41 +0300

    Merge remote-tracking branch 'origin/master'Merge remote-tracking branch 'origin/master'

--- laboratory_work_01/laboratory_work_01_02.py
+++ laboratory_work_01/laboratory_work_01_02.py
@@ -4,5 +4,6 @@ x=float(input("Введите значение для alpha: "))
 y1 = cos(x)+sin(x)+cos(3*x)+sin(3*x)
 y2 = 2*sqrt(2)*cos(x)*sin((pi/4)+2*x)
 
-print("Результат первых вычислений: ", y1)
-print("Результат вторых вычислений: ", y2)
+print("I    X         Z     I")
+print("I{0: 7.2f}  {1: 7.2f}    I".format(x, y1))
+print("I{0: 7.2f}  {1: 7.2f}    I".format(x, y2))
--- laboratory_work_02/laboratory_work_02_02.py
+++ laboratory_work_02/laboratory_work_02_02.py
@@ -1,12 +1,14 @@
+from math import *
+
 R = float(input("Введите значение для радиуса R: "))
 x = float(input("Введите значение для переменной X: "))
 y = float(input("Введите значение для переменной Y: "))
 
-if -R<=x<=0 and 0<=y<=R:
-    print("Внутри")
-elif 0<=x<R/2 and -2*x<=y<=0:
-    print("Внутри")
-elif R/2<=x<R and 2*x-2*R<=y<=0:
-    print("Внутри")
+if -R<=x<=0 and 0<=y<=sqrt(R**2 - x**2):
+    print("Точка внутри")
+elif 0<=x<=R/2 and -2*x+R<=y<=0:
+    print("Точка внутри")
+elif R/2<=x<=R and 2*x-2*R<=y<=0:
+    print("Точка внутри")
 else:
-    print("Снаружи")
+    print("Точка снаружи")
