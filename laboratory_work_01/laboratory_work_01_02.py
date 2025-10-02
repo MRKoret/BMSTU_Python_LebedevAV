a4ccb1c2c94d49b76e5837c68129005e2cb1a45e
Александр <alex_lebedmrk24@mail.ru>
2025-10-02 13:02:33 +0300

    Merge remote-tracking branch 'origin/master'Merge remote-tracking branch 'origin/master'

--- laboratory_work_01/laboratory_work_01_02.py
+++ laboratory_work_01/laboratory_work_01_02.py
@@ -4,11 +4,11 @@
 from math import sqrt, pi, sin, cos # для решения данной задачи требуется только функции корня, числа пи, синуса и косинуса
 
 
-alpha = float(input("Введите значение для alpha: "))
+alpha = float(input("Введите значение для alpha: "))# Поменял название переменной х на alpha
 
-z1 = cos(alpha) + sin(alpha) + cos(3 * alpha) + sin(3 * alpha)
+z1 = cos(alpha) + sin(alpha) + cos(3 * alpha) + sin(3 * alpha)# Поменял название переменных y1 и y2 на z1 и z2
 z2 = 2 * sqrt(2) * cos(alpha) * sin((pi / 4) + 2 * alpha)
 
-print("I    Alpha     Z     I")
+print("I    Alpha     Z     I")# Поменял вывод функции print под условие задачи
 print("I{0: 7.2f}  {1: 7.2f}    I".format(alpha, z1))
 print("I{0: 7.2f}  {1: 7.2f}    I".format(alpha, z2))
