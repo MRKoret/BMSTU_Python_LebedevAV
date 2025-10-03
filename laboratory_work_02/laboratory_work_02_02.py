84d4841293943fcdb805ddeac4a556039b087b61
Александр <alex_lebedmrk24@mail.ru>
2025-10-03 21:10:29 +0300

    Merge remote-tracking branch 'origin/master'Merge remote-tracking branch 'origin/master'

--- laboratory_work_02/laboratory_work_02_02.py
+++ laboratory_work_02/laboratory_work_02_02.py
@@ -1,14 +1,25 @@
-from math import *
+# 1. Ограничить импортируемые функции только теми, что указаны в тексте программы и отчете.
+# 2. Исправить текст программы в соответствии с требованиями Руководства по стилю кода Python
+# (https://peps.python.org/pep-0008/).
+# 3. Исправить отчет в соответствии с методическими указаниями в шаблоне отчета
+# (см. файл template_laboratory_report_00.ott).
 
-R = float(input("Введите значение для радиуса R: "))
-x = float(input("Введите значение для переменной X: "))
-y = float(input("Введите значение для переменной Y: "))
+from math import sqrt
 
-if -R<=x<=0 and 0<=y<=sqrt(R**2 - x**2):
-    print("Точка внутри")
-elif 0<=x<=R/2 and -2*x+R<=y<=0:
-    print("Точка внутри")
-elif R/2<=x<=R and 2*x-2*R<=y<=0:
-    print("Точка внутри")
-else:
-    print("Точка снаружи")
+def main():
+    radius = float(input("Введите значение для радиуса R: "))
+    x = float(input("Введите значение для переменной X: "))
+    y = float(input("Введите значение для переменной Y: "))
+
+    if (-radius <= x <= 0 and 0 <= y <= sqrt(radius ** 2 - x ** 2)):
+        print("Точка внутри")
+    elif (0 <= x <= radius / 2 and -2 * x + radius <= y <= 0):
+        print("Точка внутри")
+    elif (radius / 2 <= x <= radius and 2 * x - 2 * radius <= y <= 0):
+        print("Точка внутри")
+    else:
+        print("Точка снаружи")
+
+
+if __name__ == "__main__":
+    main()
