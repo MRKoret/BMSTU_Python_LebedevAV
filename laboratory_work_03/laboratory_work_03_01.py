78c4d70818108a6441e9f0ca7fc49692416ae66b
Александр <alex_lebedmrk24@mail.ru>
2025-10-18 10:23:17 +0300

    Merge remote-tracking branch 'origin/master'Merge remote-tracking branch 'origin/master'

--- laboratory_work_03/laboratory_work_03_01.py
+++ laboratory_work_03/laboratory_work_03_01.py
@@ -4,18 +4,19 @@
 # 3. Исправить отчет в соответствии с методическими указаниями в шаблоне отчета
 # (см. файл template_laboratory_report_00.ott).
 
-from math import sqrt # Для решения данной задачи требуется только функция sqrt
+from math import sqrt  # Для решения данной задачи требуется только функция sqrt
 
 print("Введите Xbeg, Xend и dx")
-xb=float(input("Введите Xначальное: "))
-xe=float(input("Введите Xконечное: "))
-dx=float(input("Введите шаг dx: "))
+xb = float(input("Введите Xначальное: "))
+xe = float(input("Введите Xконечное: "))
+dx = float(input("Введите шаг dx: "))
 
-y=0
-xt=xb
+y = 0.0
+
+xt = xb
 print("I    X   I    Y   I")
 print("+--------+--------+")
-while  xt<=xe:
+while xt <= xe:
     if xt <= -8:
         y = -3
     elif -8 < xt < -3:
@@ -27,5 +28,5 @@ while  xt<=xe:
     else:
         y = 3
     print("I{0: 7.2f} I{1: 7.2f} I".format(xt, y))
-    xt+=dx
+    xt += dx
 print("+--------+--------+")
